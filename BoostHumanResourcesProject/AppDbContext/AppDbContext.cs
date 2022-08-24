676d4a18f58d85d7e978a7bab73bb49510848332
Esra Ezgi Erdogan <esra.ezgi.erdogan@hotmail.com>
2022-08-24 12:52:41 +0300

    connection string oluşturuldu

---
+++ BoostHumanResourcesProject/AppDbContext/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+}
