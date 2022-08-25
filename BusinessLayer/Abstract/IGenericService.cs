ac1c98ce47ec00a067270bc7fab9e8a95c3a5b34
Berkay <bberk@gmail.com>
2022-08-25 12:47:28 +0300

    BLL Katmanı

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<TEntity>
+    {
+        void TAdd(TEntity entity);
+        void TDelete(TEntity entity);
+        void TUpdate(TEntity entity);
+        List<TEntity> GetAll();
+        TEntity TGetById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/DepartmentManager.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DepartmentManager : IDepartmentService
+    {
+        private readonly IDepartmentRepository departmentRepository;
+
+        public DepartmentManager(IDepartmentRepository _departmentRepository)
+        {
+            departmentRepository = _departmentRepository;
+        }
+        public List<Department> GetAll()
+        {
+            return departmentRepository.GetAll();
+            
+        }
+        public List<Department> GetListById(int id)
+        {
+            return departmentRepository.GetListFilter(x => x.DepartmentID == id);
+        }
+
+        public void TAdd(Department entity)
+        {
+            departmentRepository.Create(entity);
+        }
+
+        public void TDelete(Department entity)
+        {
+            departmentRepository.Delete(entity);
+        }
+
+        public Department TGetById(int id)
+        {
+            return departmentRepository.GetById(id);
+        }
+
+        public void TUpdate(Department entity)
+        {
+            departmentRepository.Update(entity);
+        }
+    }
+}
