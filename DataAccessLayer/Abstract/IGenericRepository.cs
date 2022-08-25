9a3abb7d866c63787ea82c70864bd9f13ab8ac78
Berkay <bberk@gmail.com>
2022-08-25 10:33:43 +0300

    Generic Interface

---
+++ DataAccessLayer/Abstract/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(int id);
+        List<T> GetAll();
+        List<T> GetAll(Expression<Func<T, bool>> filter);
+
+        void Create(T entity);
+        void Update(T entity); 
+        void Delete(T entity);
+    }
+}
+
+    
