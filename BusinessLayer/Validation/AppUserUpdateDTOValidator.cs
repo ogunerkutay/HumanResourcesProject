ad93e494d4f99f9d0cc018342b49ba248276429d
NGSS <cerengenlik318@gmail.com>
2022-09-01 16:30:28 +0300

    valitador kısımları düzenlendi

--- BusinessLayer/Validation/AppUserUpdateDTOValidator.cs
+++ BusinessLayer/Validation/AppUserUpdateDTOValidator.cs
@@ -8,15 +8,18 @@ namespace BusinessLayer.Validation
     {
         public AppUserUpdateDTOValidator()
         {
-            RuleFor(x => x.FirstName).NotEmpty().WithMessage("İsim boş geçilemez");
-            RuleFor(x => x.LastName).NotEmpty().WithMessage("Soyisim boş geçilemez");
-            //RuleFor(x => x.TCNO).NotEmpty().WithMessage("TC No boş geçilemez").Length(11).WithMessage("TC No 11 haneli olmalıdır")
-            //.Matches(@"^\\d + $").WithMessage("Sadece sayı girişi yapılmalıdır."); // tc 11 hane olmalı kısmı yapılcak ve hepsi rakam olacak
-                                                                                   //RuleFor(x => x.Email).NotEmpty().WithMessage("Email boş geçilemez")
-                                                                                   //.EmailAddress().WithMessage("Geçerli mail adresi yazınız");
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("İsim boş geçilemez").Matches(@"^[a-zA-Z]+$").WithMessage("Sadece harf girişi yapılmalıdır.").MinimumLength(2).WithMessage("İsim  2 harften küçük olamaz").MaximumLength(20).WithMessage("İsim 20 karakterden fazla olamaz");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Soyisim boş geçilemez").Matches(@"^[a-zA-Z]+$").WithMessage("Sadece harf girişi yapılmalıdır.").MinimumLength(2).WithMessage("İsim  2 harften küçük olamaz").MaximumLength(20).WithMessage("İsim 20 karakterden fazla olamaz");
+            RuleFor(x => x.TCNO).NotEmpty().WithMessage("TC No boş geçilemez")
+            .Matches(@"^[0-9]*$").WithMessage("Sadece sayı girişi yapılmalıdır.").Length(11).WithMessage("TC No 11 haneli olmalıdır"); // tc 11 hane olmalı kısmı yapılcak ve hepsi rakam olacak
+
+            
+            //RuleFor(x => x.Email).NotEmpty().WithMessage("Email boş geçilemez")
+            //.EmailAddress().WithMessage("Geçerli mail adresi yazınız");
             RuleFor(p => p.BirthDate)
             .NotEmpty().WithMessage("Doğum tarihi boş geçilemez")
-            .LessThan(p => DateTime.Now).WithMessage("Doğum tarihi bugünün tarihinden büyük olamaz");
+            .LessThan(p => DateTime.Now.AddYears(-18)).WithMessage("Personeller 18 yaşından küçük olamaz")
+            .GreaterThan(p => DateTime.Now.AddYears(-70)).WithMessage("Personeller 70 yaşından büyük olamaz");
             RuleFor(x => x.Address).NotEmpty().WithMessage("Adres boş geçilemez");
             //RuleFor(x => x.Status).IsInEnum().WithMessage("Çalışma durumu belirtilmelidir"); 
             RuleFor(x => x.Title).NotEmpty().WithMessage("Ünvan boş geçilemez");
