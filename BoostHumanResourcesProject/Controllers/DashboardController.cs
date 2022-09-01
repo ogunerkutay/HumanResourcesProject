0a08ac3d3d733840e01657df6bb9173b1db32b98
Esra Ezgi Erdogan <esra.ezgi.erdogan@hotmail.com>
2022-09-01 17:14:51 +0300

    yönetici düzeltildi

--- BoostHumanResourcesProject/Controllers/DashboardController.cs
+++ BoostHumanResourcesProject/Controllers/DashboardController.cs
@@ -1,26 +1,33 @@
-﻿using DataAccessLayer;
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Models.VMs;
+using DataAccessLayer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace BoostHumanResourcesProject.Controllers
 {
     public class DashboardController : Controller
     {
         private readonly AppDbContext appDbContext;
+        private readonly IAppUserService appUserService;
         //protected DbSet<T> table;
-        public DashboardController(AppDbContext _appDbContext)
+        public DashboardController(AppDbContext appDbContext, IAppUserService appUserService)
         {
-            appDbContext = _appDbContext;
+            this.appDbContext = appDbContext;
+            this.appUserService = appUserService;
             //table = _appDbContext.Set<T>();
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            AppUserDetailsVM appUserDetailsVM = await appUserService.GetById(8);
+            
             ViewBag.v1 = appDbContext.AppUsers.Count().ToString();
             ViewBag.v2 = appDbContext.Departments.Count().ToString();
             ViewBag.v3 = appDbContext.DayOffs.Count().ToString();
-            return View();
+            return View(appUserDetailsVM);
         }
     }
 }
