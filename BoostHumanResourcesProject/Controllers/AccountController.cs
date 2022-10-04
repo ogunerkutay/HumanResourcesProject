a86eb67c3c302e35418992667a385d94ef4228d5
Esra Ezgi Erdogan <esra.ezgi.erdogan@hotmail.com>
2022-10-04 11:52:01 +0300

    şifremi unuttum düzenlemeleri

--- BoostHumanResourcesProject/Controllers/AccountController.cs
+++ BoostHumanResourcesProject/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using BusinessLayer.Abstract;
+using BusinessLayer.Email;
 using BusinessLayer.Models.DTOs;
 using BusinessLayer.Models.VMs;
 using BusinessLayer.Services.AppUserService;
@@ -18,11 +19,13 @@ namespace BoostHumanResourcesProject.Areas.Personel.Controllers
     public class AccountController : Controller
     {
         private readonly IAppUserService appUserService;
+        private readonly UserManager<AppUser> userManager;
 
 
-        public AccountController(IAppUserService appUserService)
+        public AccountController(IAppUserService appUserService, UserManager<AppUser> userManager)
         {
             this.appUserService = appUserService;
+            this.userManager = userManager;
         }
         //
 
@@ -59,6 +62,33 @@ namespace BoostHumanResourcesProject.Areas.Personel.Controllers
                 if (user != null) return RedirectToAction("Login", "Account");
             }
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return View();
+            }
+            var user = await userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                var code = await userManager.GeneratePasswordResetTokenAsync(user);
+                var urlForForgotPassword = Url.Action("ResetPassword", "Account", new {
+                userId = user.Id, token=code});
+                EmailHelper emailHelper = new EmailHelper();
+                string message = $"Şifreniz: {user.FirstName}123Aa! Lütfen unutmayın, değiştirmeyi de ihmal etmeyin!!!";
+                bool emailResponse = emailHelper.SendEmail(user.Email, message);
+                return emailResponse;
+            }
+            return View();
         }
 
 
